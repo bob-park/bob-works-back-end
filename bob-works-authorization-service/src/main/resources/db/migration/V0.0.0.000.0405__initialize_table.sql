1f1025537206c6c65e0e8c618c185131c27bf0be
Bob Park <hwpark11@kakao.com>
2023-04-05 22:35:31 +0900

    feat: authorization client 관련 table ddl 추가

---
+++ bob-works-authorization-service/src/main/resources/db/migration/V0.0.0.000.0405__initialize_table.sql
@@ -0,0 +1,49 @@
+-- create table
+
+-- authorization_scopes
+create table authorization_scopes
+(
+    id                 bigserial               not null primary key,
+    scope              varchar(100)            not null,
+    created_date       timestamp default now() not null,
+    last_modified_date timestamp
+);
+
+
+-- authorization_clients
+create table authorization_clients
+(
+    id                             bigserial               not null primary key,
+    client_id                      varchar(100)            not null,
+    client_secret                  varchar(1000)           not null,
+    client_issue_at                timestamp               not null,
+    client_secret_expires_at       timestamp,
+    required_authorization_consent bool      default false not null,
+    access_token_time_to_live      bigint                  not null,
+    created_date                   timestamp default now() not null,
+    last_modified_date             timestamp
+);
+
+-- authorization_client_redirect_url
+create table authorization_clients_redirects
+(
+    id                 serial                  not null primary key,
+    client_id          bigint                  not null,
+    redirect_uri       varchar(1000)           not null,
+    created_date       timestamp default now() not null,
+    last_modified_date timestamp,
+
+    foreign key (client_id) references authorization_clients (id)
+);
+
+-- authorization_clients_scopes
+create table authorization_clients_scopes
+(
+    id        bigserial not null primary key,
+    client_id bigint    not null,
+    scope_id  bigint    not null,
+
+    foreign key (client_id) references authorization_clients (id),
+    foreign key (scope_id) references authorization_scopes (id)
+);
+
