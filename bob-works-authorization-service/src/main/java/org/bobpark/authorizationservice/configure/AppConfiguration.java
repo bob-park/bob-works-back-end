c6188ee8775af1b80b4c69c5eaf2aede349a486e
Bob Park <hwpark@malgn.com>
2023-04-06 16:17:27 +0900

    feat: configuration 추가 및 base time entity 추가

---
+++ bob-works-authorization-service/src/main/java/org/bobpark/authorizationservice/configure/AppConfiguration.java
@@ -0,0 +1,9 @@
+package org.bobpark.authorizationservice.configure;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+@Configuration
+public class AppConfiguration {
+}
---
+++ bob-works-authorization-service/src/main/java/org/bobpark/authorizationservice/configure/jpa/JpaConfiguration.java
@@ -0,0 +1,9 @@
+package org.bobpark.authorizationservice.configure.jpa;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfiguration {
+}
