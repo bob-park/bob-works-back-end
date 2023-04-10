f356f6e07d7b61474b4be73ced2419ad3d9b8a11
Bob Park <hwpark11@kakao.com>
2023-04-10 21:42:07 +0900

    feat: role hierarchy + authorizationManager 추가

--- bob-works-authorization-service/src/main/java/org/bobpark/authorizationservice/configure/security/DefaultSecurityConfiguration.java
+++ bob-works-authorization-service/src/main/java/org/bobpark/authorizationservice/configure/security/DefaultSecurityConfiguration.java
@@ -1,8 +1,17 @@
 package org.bobpark.authorizationservice.configure.security;
 
+import java.util.List;
+import java.util.Map;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyUtils;
+import org.springframework.security.authorization.AuthorityAuthorizationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -11,10 +20,17 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 
+import org.bobpark.authorizationservice.domain.role.service.RoleHierarchyService;
+
+@Slf4j
+@RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class DefaultSecurityConfiguration {
+
+    private final RoleHierarchyService roleHierarchyService;
 
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -42,6 +58,35 @@ public class DefaultSecurityConfiguration {
                 .build();
 
         return new InMemoryUserDetailsManager(user);
+    }
+
+    /*
+      role hierarchy
+     */
+    @Bean
+    public AuthorityAuthorizationManager<RequestAuthorizationContext> authorizationManager() {
+
+        AuthorityAuthorizationManager<RequestAuthorizationContext> authorizationManager =
+            AuthorityAuthorizationManager.hasAnyAuthority("USER");
+
+        authorizationManager.setRoleHierarchy(roleHierarchy());
+
+        return authorizationManager;
+    }
+
+    @Bean
+    public RoleHierarchyImpl roleHierarchy() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+
+        Map<String, List<String>> roleHierarchyMap = roleHierarchyService.getRoleHierarchyToMap();
+
+        String rolesHierarchyStr = RoleHierarchyUtils.roleHierarchyFromMap(roleHierarchyMap);
+
+        log.debug("role hierarchy={}", rolesHierarchyStr);
+
+        roleHierarchy.setHierarchy(rolesHierarchyStr);
+
+        return roleHierarchy;
     }
 
 }
