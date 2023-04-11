5076c14bbb6f8f4c86cd7ec4c455cfd3d6a0f297
Bob Park <hwpark@malgn.com>
2023-04-11 16:46:48 +0900

    feat: jpa + resource server 설정 추가

--- bob-works-document-service/src/main/java/org/bobpark/documentservice/DocumentServiceApplication.java
+++ bob-works-document-service/src/main/java/org/bobpark/documentservice/DocumentServiceApplication.java
@@ -1,4 +1,4 @@
-package org.bobpark.bobworksdocumentservice;
+package org.bobpark.documentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
