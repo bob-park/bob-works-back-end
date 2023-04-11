a6601db347c62a4646303b28ea50e359e5bcf6e9
Bob Park <hwpark11@kakao.com>
2023-04-12 01:21:35 +0900

    feat: resource server 설정에 jwt scope converter + role hierarchy 추가

--- bob-works-document-service/src/main/java/org/bobpark/documentservice/configure/security/OAuth2ResourceServerConfiguration.java
+++ bob-works-document-service/src/main/java/org/bobpark/documentservice/configure/security/OAuth2ResourceServerConfiguration.java
@@ -1,24 +1,95 @@
 package org.bobpark.documentservice.configure.security;
 
+import java.util.List;
+import java.util.Map;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyUtils;
+import org.springframework.security.authorization.AuthorityAuthorizationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 
+import org.bobpark.documentservice.configure.security.converter.JwtRoleGrantAuthoritiesConverter;
+import org.bobpark.documentservice.domain.role.service.RoleHierarchyService;
+
+@Slf4j
+@RequiredArgsConstructor
+@EnableMethodSecurity
 @Configuration
 public class OAuth2ResourceServerConfiguration {
 
+    private final RoleHierarchyService roleHierarchyService;
+
     @Bean
-    public SecurityFilterChain securityFilterChain1(HttpSecurity http) throws Exception {
+    public SecurityFilterChain resourceSecurityFilterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests(
             requests ->
                 requests.anyRequest().authenticated());
 
-        http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        http.oauth2ResourceServer(
+            resourceServer ->
+                resourceServer.jwt(
+                    jwtConfigurer ->
+                        jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter())));
 
         return http.build();
     }
 
+    @Bean
+    static MethodSecurityExpressionHandler expressionHandler(RoleHierarchy roleHierarchy) {
+        DefaultMethodSecurityExpressionHandler expressionHandler = new DefaultMethodSecurityExpressionHandler();
+
+        expressionHandler.setRoleHierarchy(roleHierarchy);
+
+        return expressionHandler;
+    }
+
+    /*
+      role hierarchy
+     */
+    @Bean
+    public AuthorityAuthorizationManager<RequestAuthorizationContext> authorizationManager() {
+
+        AuthorityAuthorizationManager<RequestAuthorizationContext> authorizationManager =
+            AuthorityAuthorizationManager.hasRole("USER");
+
+        authorizationManager.setRoleHierarchy(roleHierarchy());
+
+        return authorizationManager;
+    }
+
+    @Bean
+    public RoleHierarchyImpl roleHierarchy() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+
+        Map<String, List<String>> roleHierarchyMap = roleHierarchyService.getRoleHierarchyToMap();
+
+        String rolesHierarchyStr = RoleHierarchyUtils.roleHierarchyFromMap(roleHierarchyMap);
+
+        log.debug("role hierarchy={}", rolesHierarchyStr);
+
+        roleHierarchy.setHierarchy(rolesHierarchyStr);
+
+        return roleHierarchy;
+    }
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new JwtRoleGrantAuthoritiesConverter());
+
+        return jwtAuthenticationConverter;
+    }
 }
