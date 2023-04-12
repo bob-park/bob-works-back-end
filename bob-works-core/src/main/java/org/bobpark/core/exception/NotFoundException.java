d2372677ef41e10d47026da47e3d99dc3fb894c4
Bob Park <hwpark@malgn.com>
2023-04-12 14:49:59 +0900

    feat: 기본 exception 추가

---
+++ bob-works-core/src/main/java/org/bobpark/core/exception/NotFoundException.java
@@ -0,0 +1,27 @@
+package org.bobpark.core.exception;
+
+import java.text.ChoiceFormat;
+import java.text.MessageFormat;
+
+import org.bobpark.core.model.common.Id;
+
+public class NotFoundException extends RuntimeException {
+
+    private static final MessageFormat DEFAULT_FORMAT = new MessageFormat("Not found ''{1}'' with query values ''{0,number,#}''");
+
+    public NotFoundException(String message) {
+        super("Not found - " + message);
+    }
+
+    public NotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public NotFoundException(String message, Throwable cause) {
+        super("Not found - " + message, cause);
+    }
+
+    public <R, T> NotFoundException(Id<R, T> id) {
+        super(DEFAULT_FORMAT.format(new Object[] {id.getValue(), id.getClazz().getSimpleName()}));
+    }
+}
