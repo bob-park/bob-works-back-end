7343a19c308f573f685d3eaf6c6b3dc244d9e3eb
Bob Park <hwpark11@kakao.com>
2023-04-13 23:20:28 +0900

    refactor: authorization client 조회시 client secret 복호화 진행

--- bob-works-authorization-service/src/main/java/org/bobpark/authorizationservice/configure/security/DefaultSecurityConfiguration.java
+++ bob-works-authorization-service/src/main/java/org/bobpark/authorizationservice/configure/security/DefaultSecurityConfiguration.java
@@ -18,6 +18,9 @@ import org.springframework.security.access.hierarchicalroles.RoleHierarchyUtils;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
@@ -84,6 +87,11 @@ public class DefaultSecurityConfiguration {
     @Bean
     public AccessDeniedHandler accessDeniedHandler() {
         return new RestAccessDeniedHandler(om);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
 }
