243f24d853996bb2236cbf65e1fb807ba7800d89
Bob Park <hwpark@malgn.com>
2023-04-13 17:22:47 +0900

    feat: documents 관련 table ddl 추가

---
+++ bob-works-document-service/src/main/resources/db/migration/V0.0.1.000.0413__add_document_table.sql
@@ -0,0 +1,47 @@
+/*
+    create table
+ */
+-- documents
+create table documents
+(
+    id                 bigserial                     not null primary key,
+    type               varchar(20)                   not null,
+    type_id            bigint                        not null,
+    writer_id          bigint                        not null,
+    status             varchar(20) default 'WAITING' not null,
+    created_date       timestamp   default now()     not null,
+    created_by         varchar(100)                  not null,
+    last_modified_date timestamp,
+    last_modified_by   varchar(100),
+
+    foreign key (type_id) references document_types (id),
+    foreign key (writer_id) references users (id)
+);
+
+-- documents_vacations
+create table documents_vacations
+(
+    document_id        bigint       not null primary key,
+    vacation_type      varchar(20)  not null,
+    vacation_sub_type  varchar(20),
+    vacation_date_from date         not null,
+    vacation_date_to   date         not null,
+    days_count         real         not null,
+    reason             varchar(255) not null,
+
+    foreign key (document_id) references documents (id)
+);
+
+-- documents_approvals
+create table document_approvals
+(
+
+    id                 bigserial                     not null primary key,
+    line_id            bigint                        not null,
+    status             varchar(20) default 'WAITING' not null,
+    approved_time      timestamp,
+    created_date       timestamp   default now()     not null,
+    last_modified_date timestamp,
+
+    foreign key (line_id) references document_types_approve_lines (id)
+);
