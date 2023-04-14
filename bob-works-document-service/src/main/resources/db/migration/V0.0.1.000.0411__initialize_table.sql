75796bf57e2efda3608f124ec8c6191455b0656e
Bob Park <hwpark11@kakao.com>
2023-04-14 21:41:46 +0900

    feat: document service 에서 user table ddl 분리

--- bob-works-document-service/src/main/resources/db/migration/V0.0.1.000.0411__initialize_table.sql
+++ bob-works-document-service/src/main/resources/db/migration/V0.0.1.000.0411__initialize_table.sql
@@ -1,28 +1,6 @@
 /*
     create table
  */
--- positions
-create table positions
-(
-    id                 bigserial               not null primary key,
-    name               varchar(100)            not null,
-    created_date       timestamp default now() not null,
-    created_by         varchar(100)            not null,
-    last_modified_date timestamp,
-    last_modified_by   varchar(100)
-);
-
--- users_positions
-create table users_positions
-(
-    id          bigserial not null primary key,
-    user_id     bigint    not null,
-    position_id bigint    not null,
-
-    foreign key (user_id) references users (id),
-    foreign key (position_id) references positions (id)
-);
-
 -- document_types
 create table document_types
 (
@@ -52,20 +30,6 @@ create table document_types_approval_lines
 /*
     insert data
  */
--- positions
-insert into positions (id, name, created_by) values (11, '사원', 'admin');
-insert into positions (id, name, created_by) values (12, '대리', 'admin');
-insert into positions (id, name, created_by) values (13, '과장', 'admin');
-insert into positions (id, name, created_by) values (14, '차장', 'admin');
-insert into positions (id, name, created_by) values (15, '부장', 'admin');
-insert into positions (id, name, created_by) values (16, '팀장', 'admin');
-insert into positions (id, name, created_by) values (17, '이사', 'admin');
-
--- users_position
-insert into users_positions(user_id, position_id) values (11, 17);
-insert into users_positions(user_id, position_id) values (12, 16);
-insert into users_positions(user_id, position_id) values (13, 11);
-
 -- document_types
 insert into document_types (id, type, name, created_by) values (1, 'VACATION', '휴가계', 'admin');
 
