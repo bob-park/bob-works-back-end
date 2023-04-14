82beaab455c40d5c6c395f9f1b602c5741f26e84
Bob Park <hwpark11@kakao.com>
2023-04-15 00:15:25 +0900

    refactor: 사용하지 않는 소스 제거

--- bob-works-document-service/src/main/java/org/bobpark/documentservice/domain/document/service/impl/DocumentServiceImpl.java
+++ bob-works-document-service/src/main/java/org/bobpark/documentservice/domain/document/service/impl/DocumentServiceImpl.java
@@ -9,7 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -29,11 +28,7 @@ import org.bobpark.documentservice.domain.user.model.UserResponse;
 @Transactional(readOnly = true)
 public class DocumentServiceImpl implements DocumentService {
 
-    private static final String ROLE_NAME_MANAGER = "ROLE_MANAGER";
-
     private final DocumentRepository documentRepository;
-
-    private final RoleHierarchy roleHierarchy;
 
     @Override
     public Page<DocumentResponse> search(SearchDocumentRequest searchRequest, Pageable pageable) {
