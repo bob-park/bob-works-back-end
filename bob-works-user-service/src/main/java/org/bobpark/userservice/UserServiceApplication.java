29c819c3d67a2e396d984d4460df362098c2f28a
Bob Park <hwpark11@kakao.com>
2023-04-14 21:39:57 +0900

    feat: user service 모듈 추가

---
+++ bob-works-user-service/src/main/java/org/bobpark/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package org.bobpark.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
