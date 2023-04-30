fb68a4dcb0964f55dec6cbd666f246fd1997e701
Bob Park <hwpark11@kakao.com>
2023-04-30 22:00:15 +0900

    feat: document service 에 사용자 팀 정보 추가

--- bob-works-document-service/src/main/java/org/bobpark/documentservice/domain/user/model/UserResponse.java
+++ bob-works-document-service/src/main/java/org/bobpark/documentservice/domain/user/model/UserResponse.java
@@ -1,12 +1,14 @@
 package org.bobpark.documentservice.domain.user.model;
 
 import org.bobpark.documentservice.domain.position.model.PositionResponse;
+import org.bobpark.documentservice.domain.team.model.TeamResponse;
 import org.bobpark.documentservice.domain.user.model.vacation.UserVacationResponse;
 
 public record UserResponse(Long id,
                            String userId,
                            String email,
                            String name,
+                           TeamResponse team,
                            PositionResponse position,
                            UserVacationResponse nowVacation) {
 }
