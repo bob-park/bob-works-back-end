3ed2e66b3ed5f9208ba7340ac399e7a4e60e694c
Bob Park <hwpark11@kakao.com>
2023-05-10 21:53:32 +0900

    feat: user avatar table ddl 추가

---
+++ bob-works-user-service/src/main/resources/db/migration/V0.0.1.002.0510__add-users-avatars-table.sql
@@ -0,0 +1,11 @@
+-- users_avatars
+create table users_avatars
+(
+    id               bigserial    not null primary key,
+    user_id          bigint       not null,
+    avatar_path      varchar(1000),
+    created_date     timestamp    not null default now(),
+    created_by       varchar(100) not null,
+    last_modify_date timestamp,
+    last_modify_by   varchar(100)
+);
