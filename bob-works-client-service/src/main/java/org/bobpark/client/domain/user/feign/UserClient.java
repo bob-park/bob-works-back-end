a65460ef70a5ec4218ce1f0c85c2cc155e988ff8
Bob Park <hwpark11@kakao.com>
2023-05-15 23:05:08 +0900

    refactor: feign client 에서 @RequestBody 제거

--- bob-works-client-service/src/main/java/org/bobpark/client/domain/user/feign/UserClient.java
+++ bob-works-client-service/src/main/java/org/bobpark/client/domain/user/feign/UserClient.java
@@ -4,7 +4,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import org.bobpark.client.domain.user.model.UpdateUserPasswordRequest;
@@ -13,12 +12,10 @@ import org.bobpark.client.domain.user.model.UserResponse;
 @FeignClient(name = "user-service", path="user")
 public interface UserClient {
 
-    // @GetMapping(path = "user")
     @GetMapping(path = "")
     UserResponse getUser(@RequestParam("userId") String userId);
 
     @PutMapping(path = "{id}/password")
-    // @PutMapping(path = "user/{id}/password")
-    UserResponse updatePassword(@PathVariable long id, @RequestBody UpdateUserPasswordRequest updateRequest);
+    UserResponse updatePassword(@PathVariable long id, UpdateUserPasswordRequest updateRequest);
 
 }
