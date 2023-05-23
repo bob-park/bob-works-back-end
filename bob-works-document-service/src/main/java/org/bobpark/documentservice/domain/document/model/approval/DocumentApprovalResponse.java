b5d9c2705dea71c3021212dd9119adc0265a875e
Bob Park <hwpark11@kakao.com>
2023-05-23 21:19:24 +0900

    feat: dto 에 결제 사용자 아이디 추가

--- bob-works-document-service/src/main/java/org/bobpark/documentservice/domain/document/model/approval/DocumentApprovalResponse.java
+++ bob-works-document-service/src/main/java/org/bobpark/documentservice/domain/document/model/approval/DocumentApprovalResponse.java
@@ -1,19 +1,18 @@
 package org.bobpark.documentservice.domain.document.model.approval;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import lombok.Builder;
 
 import org.bobpark.documentservice.domain.document.entity.DocumentApproval;
 import org.bobpark.documentservice.domain.document.model.DocumentResponse;
 import org.bobpark.documentservice.domain.document.type.DocumentStatus;
-import org.bobpark.documentservice.domain.user.model.UserResponse;
 
 @Builder
 public record DocumentApprovalResponse(Long id,
                                        DocumentResponse document,
                                        Long lineId,
+                                       Long signUserId,
                                        DocumentStatus status,
                                        LocalDateTime approvedDateTime,
                                        String reason) {
@@ -22,6 +21,7 @@ public record DocumentApprovalResponse(Long id,
         return DocumentApprovalResponse.builder()
             .id(approval.getId())
             .lineId(approval.getApprovalLine().getId())
+            .signUserId(approval.getApprovalLine().getUserId())
             .status(approval.getStatus())
             .approvedDateTime(approval.getApprovedDateTime())
             .reason(approval.getReason())
