09821fd8bcc57ed992c4b387166b602ab31ff4af
Bob Park <hwpark11@kakao.com>
2023-06-05 22:22:13 +0900

    feat: document 취소 기능 추가

--- bob-works-document-service/src/main/java/org/bobpark/documentservice/domain/document/controller/DocumentController.java
+++ bob-works-document-service/src/main/java/org/bobpark/documentservice/domain/document/controller/DocumentController.java
@@ -6,10 +6,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.bobpark.core.model.common.Id;
+import org.bobpark.documentservice.domain.document.entity.Document;
 import org.bobpark.documentservice.domain.document.model.DocumentResponse;
 import org.bobpark.documentservice.domain.document.model.SearchDocumentRequest;
 import org.bobpark.documentservice.domain.document.service.DocumentService;
@@ -25,6 +31,12 @@ public class DocumentController {
     public Page<DocumentResponse> search(SearchDocumentRequest searchRequest,
         @PageableDefault(sort = "createdDate", direction = Direction.DESC) Pageable pageable) {
         return documentService.search(searchRequest, pageable);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping(path = "{documentId:\\d+}/cancel")
+    public DocumentResponse cancel(@PathVariable long documentId) {
+        return documentService.cancel(Id.of(Document.class, documentId));
     }
 
 }
