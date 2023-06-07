018c9df3acde4e66e90ebe208c9c6ca32606838b
Bob Park <hwpark11@kakao.com>
2023-06-07 20:11:30 +0900

    feat: document approval fetch join 조회하는 메서드 추가

--- bob-works-document-service/src/main/java/org/bobpark/documentservice/domain/document/repository/approval/query/DocumentApprovalQueryRepository.java
+++ bob-works-document-service/src/main/java/org/bobpark/documentservice/domain/document/repository/approval/query/DocumentApprovalQueryRepository.java
@@ -1,6 +1,7 @@
 package org.bobpark.documentservice.domain.document.repository.approval.query;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +12,8 @@ import org.bobpark.documentservice.domain.document.entity.DocumentApproval;
 import org.bobpark.documentservice.domain.document.model.approval.SearchDocumentApprovalRequest;
 
 public interface DocumentApprovalQueryRepository {
+
+    Optional<DocumentApproval> findById(Id<DocumentApproval, Long> approvalId);
 
     Page<DocumentApproval> search(SearchDocumentApprovalRequest searchRequest, Pageable pageable);
 
