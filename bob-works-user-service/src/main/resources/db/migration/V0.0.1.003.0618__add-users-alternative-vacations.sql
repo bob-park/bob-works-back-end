8f106be2cba4c7d0df1f05fe0de9ea102dac8f60
Bob Park <hwpark11@kakao.com>
2023-06-18 17:51:15 +0900

    feat: 사용자 대체 휴가 목록 테이블 ddl 추가

---
+++ bob-works-user-service/src/main/resources/db/migration/V0.0.1.003.0618__add-users-alternative-vacations.sql
@@ -0,0 +1,19 @@
+-- create table
+
+-- users_alternative_vacations
+create table users_alternative_vacations
+(
+    id                 bigserial               not null primary key,
+    user_id            bigint                  not null,
+    effective_date     date                    not null,
+    effective_count    real      default 0     not null,
+    use_count         real      default 0     not null,
+    is_expired         bool      default false not null,
+    effective_reason   varchar(1000)           not null,
+    created_date       timestamp default now() not null,
+    created_by         varchar(100)            not null,
+    last_modified_date timestamp,
+    last_modified_by   varchar(100),
+
+    foreign key (user_id) references users (id)
+);
