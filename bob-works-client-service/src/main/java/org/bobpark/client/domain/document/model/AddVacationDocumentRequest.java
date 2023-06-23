3249a0429f82a796e2999de13bda124a646cc513
Bob Park <hwpark11@kakao.com>
2023-06-24 00:50:35 +0900

    feat: 대체 휴가 신청시 대체 휴가 아이디 목록 추가

--- bob-works-client-service/src/main/java/org/bobpark/client/domain/document/model/AddVacationDocumentRequest.java
+++ bob-works-client-service/src/main/java/org/bobpark/client/domain/document/model/AddVacationDocumentRequest.java
@@ -1,11 +1,13 @@
 package org.bobpark.client.domain.document.model;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public record AddVacationDocumentRequest(Long typeId,
                                          String vacationType,
                                          String vacationSubType,
                                          LocalDate vacationDateFrom,
                                          LocalDate vacationDateTo,
-                                         String reason) {
+                                         String reason,
+                                         List<Long> useAlternativeVacationIds) {
 }
