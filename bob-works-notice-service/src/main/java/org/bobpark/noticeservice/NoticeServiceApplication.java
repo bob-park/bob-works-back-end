a69d4358ac6585031f044c160d04033b3e77b1bd
Bob Park <hwpark11@kakao.com>
2023-07-10 20:23:38 +0900

    feat: notice service 추가

--- bob-works-notice-service/src/main/java/org/bobpark/noticeservice/NoticeServiceApplication.java
+++ bob-works-notice-service/src/main/java/org/bobpark/noticeservice/NoticeServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BobWorksNoticeServiceApplication {
+public class NoticeServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BobWorksNoticeServiceApplication.class, args);
+        SpringApplication.run(NoticeServiceApplication.class, args);
     }
 
 }
