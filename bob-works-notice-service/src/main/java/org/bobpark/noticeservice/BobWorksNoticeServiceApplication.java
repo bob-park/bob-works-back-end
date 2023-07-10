fb8b3c972fb07f8332382a8d0732bbce92fb49cb
Bob Park <hwpark11@kakao.com>
2023-07-10 20:22:05 +0900

    feat: notice service 추가

---
+++ bob-works-notice-service/src/main/java/org/bobpark/noticeservice/BobWorksNoticeServiceApplication.java
@@ -0,0 +1,13 @@
+package org.bobpark.noticeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobWorksNoticeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BobWorksNoticeServiceApplication.class, args);
+    }
+
+}
---
+++ bob-works-notice-service/src/test/java/org/bobpark/noticeservice/BobWorksNoticeServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package org.bobpark.noticeservice;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobWorksNoticeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
