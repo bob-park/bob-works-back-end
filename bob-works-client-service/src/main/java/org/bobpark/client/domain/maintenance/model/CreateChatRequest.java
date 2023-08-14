0a606c4bc2538a918d683c5634d2e341704e6ca4
Bob Park <hwpark11@kakao.com>
2023-08-14 22:39:29 +0900

    feat: chat 서비스 기능 추가

---
+++ bob-works-client-service/src/main/java/org/bobpark/client/domain/maintenance/model/CreateChatRequest.java
@@ -0,0 +1,4 @@
+package org.bobpark.client.domain.maintenance.model;
+
+public record CreateChatRequest(String contents) {
+}
---
+++ bob-works-client-service/src/main/java/org/bobpark/client/domain/maintenance/service/CustomerChatService.java
@@ -0,0 +1,14 @@
+package org.bobpark.client.domain.maintenance.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class CustomerChatService {
+
+
+}
